dea854d6aa59417a4495b12981f00a852eff54ca
MM4096 <85976396+MM4096@users.noreply.github.com>
2022-04-13 13:37:40 +1200

    game devlop

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 var c = document.getElementById("canvas");
-    var canvas = c.getContext("2d");
+var canvas = c.getContext("2d");
 var xSlice = 0;
 var ySlice = 0;
 
 $(document).ready(function() {
+    canvas.clearRect(0, 0, 1000, 750);
+    drawScreen()
 })
-
+function drawScreen() {
+    canvas.clearRect(0, 0, 1000, 750);
+    drawBackground();
+    drawPlayer();
+}
+function drawPlayer() {
+    canvas.beginPath();
+    canvas.rect(495, 370, 10, 10);
+    canvas.fillStyle = "#000000";
+    canvas.fill();
+}
 function drawBackground() {
     var img = document.getElementById("map1");
-    canvas.clearRect(0, 0, 1000, 750);
     canvas.drawImage(img, xSlice, ySlice, 21.875, 12.5, 0, 0, 1000, 750);
+    drawPlayer();
 }
 
 document.addEventListener("keydown", function(e) {
@@ -17,24 +29,25 @@ document.addEventListener("keydown", function(e) {
     if (pressed == "w") {
         if (ySlice > 0) {
             ySlice -= 0.1;
-            drawBackground();
+            drawScreen()
+            
         }
     } else if (pressed == "a") {
         if (xSlice > 0) {
             xSlice -= 0.1;
-            drawBackground();
+            drawScreen()
         }
         
     } else if (pressed == "s") {
         if (ySlice < 387) {
             ySlice += 0.1;
-            drawBackground();
+            drawScreen()
         }
 
     } else if (pressed == "d") {
         if (xSlice < 678) {
             xSlice += 0.1;
-            drawBackground();
+            drawScreen()
         }
 
     }
